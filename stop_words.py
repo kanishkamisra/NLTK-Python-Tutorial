5ae9daad93cfb7245a9ee90984ee5b70c58fae9b
vincentvangoat <menogetusername@gmail.com>
2015-10-30 22:55:33 -0400

    Started working with stop words

--- stop_words.py
+++ stop_words.py
@@ -1,1 +1,15 @@
-import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize as wt
+
+example_sentence = "This is an example showing off stop word filteration"
+stop_words = set(stopwords.words("english"))
+ # print stop_words
+
+words = wt(example_sentence)
+filtered_sentence = []
+
+for w in words:
+	if w not in stop_words:
+ 		filtered_sentence.append(w)
+
+print filtered_sentence
